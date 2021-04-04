e71b66450fb9a2745a33986f8755ad14b00dd3af
Spenny1068 <spncrlll838@gmail.com>
2021-04-03 20:03:56 -0700

    wee

--- visualize.py
+++ visualize.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
+from random import randrange
 import matplotlib.pyplot as plt
 from matplotlib import animation
+from matplotlib.patches import Circle, Rectangle
+from matplotlib.patches import Rectangle
 from pathlib import Path
+# import numpy as np
+
+
+class Cell(object):
+    """ A cell in a maze
+    """
+    def __init__(self, loc, value, color):
+        self.location = loc     # this is the same as the loc in the maze... need?
+        self.value = value
+        self.active = False
+        self.color = color
+        self.alpha = 0.4
 
 class Maze:
     """ A maze is a 2-Dimensional grid of Cell objects.
     """
     def __init__(self):
         self.id = 1
+        self.num_rows = 0
+        self.num_cols = 0
         self.maze = None
 
     def GenerateMaze(self, filename):
@@ -20,55 +37,95 @@ class Maze:
         # first line of maze instance: #rows #columns
         f = open(filename)
         line = f.readline()
-        num_rows, num_columns = [int(x) for x in line.split(' ')]
-        print("num_rows = ", str(num_rows), ", num_columns = ", str(num_columns))
+        self.num_rows, self.num_cols = [int(x) for x in line.split(' ')]
+        print("num_rows = ", str(self.num_rows), ", num_columns = ", str(self.num_cols))
 
         self.maze = []
 
         # generate maze of Cell objects
-        for i in range(num_rows):
+        for row in range(self.num_rows):
             line = f.readline()
-            for c in line:
-                self.maze.append([])
-                if c == "#":
-                    self.maze[-1].append(Cell("#"))
-                elif c == ".":
-                    self.maze[-1].append(Cell("."))
+            self.maze.append([])
+            for col, value in enumerate(line):
 
-        # print(self.maze)
+                if value == "#":
+                    self.maze[row].append(Cell([row, col], "#", "black"))
+                elif value == ".":
+                    self.maze[row].append(Cell([row, col], ".", "white"))
+                elif value == "0" or value == "1":
+                    self.maze[row].append(Cell([row, col], "A", "green"))
+                # else value == " ":
+                #     self.maze[row].append(Cell([row, col], " ", "white"))
+
+        return self.maze
 
 
-class Cell(object):
-    """ A cell in a maze
-    """
-    def __init__(self, value):
-        self.value = value
-
-class Visualize(object):
+class Visualize():
     """ Visualize and animate a maze
     """
-    def __init__(self):
-        pass
 
-    def SetupPlot(self):
+    def __init__(self, maze_obj):
+        self.ax = None
+        self.patches = []
+        self.fig = None
+        self.cell_width = 1
+        self.maze_obj = maze_obj
+        self.animation = None
+        # self.maze_obj.maze = np.flip(np.transpose(self.maze_obj.maze), 1)
+
+    def SetupFigure(self):
         """ setup the properties of the maze plot.
         """
 
-        # # aspect = len(self.my_map) / len(self.my_map[0])
-        # aspect = 10
-        # self.fig = plt.figure(figsize=(4 * aspect, 4))
+        self.fig = plt.figure(figsize = (8, 8))
 
-        # # figure title
-        # title_box = self.ax.text(0, self.maze.num_rows + self.cell_size + 0.1,
+        # axes
+        self.ax = plt.axes()
+        plt.xlim([0, self.maze_obj.num_cols])
+        plt.ylim([0, self.maze_obj.num_rows])
 
-        # self.animation = animation.FuncAnimation(self.fig, self.animate_func, frames=int(self.T + 1) * 10, interval=100, blit=True)
-        pass
+        # Set an equal aspect ratio
+        self.ax.set_aspect("equal")
+
+        # Remove the axes from the figure
+        # self.ax.axes.get_xaxis().set_visible(False)
+        # self.ax.axes.get_yaxis().set_visible(False)
+
+        # animate figure
+        self.animation = animation.FuncAnimation(self.fig, self.UpdateAnimation,
+                                                 frames=100,
+                                                 interval=100,
+                                                 blit=True)
+
+    def UpdateAnimation(self, frame):
+
+        rand_row = randrange(self.maze_obj.num_rows)
+        rand_col = randrange(self.maze_obj.num_cols)
+
+        self.maze_obj.maze[rand_row][rand_col].color = "red"
+        self.UpdateFigure()
+
+        return self.patches
+
+    def UpdateFigure(self):
+
+        self.patches.clear()
+        for i in range(self.maze_obj.num_rows):
+            for j in range(self.maze_obj.num_cols):
+                square = plt.Rectangle((j * self.cell_width, self.maze_obj.num_rows - (i * self.cell_width)), self.cell_width, self.cell_width, fc = self.maze_obj.maze[i][j].color, alpha = self.maze_obj.maze[i][j].alpha)
+                self.patches.append(square)
+
+        for p in self.patches:
+            self.ax.add_patch(p)
+
+    def ShowFigure(self):
+        plt.show()
 
 
-maze = Maze()
-maze.GenerateMaze("maze_instances/maze1.txt")
+my_maze = Maze()
+my_maze.GenerateMaze("maze_instances/maze2.txt")
 
-
-
-
-
+v = Visualize(my_maze)
+v.SetupFigure()
+v.UpdateFigure()
+v.ShowFigure()
