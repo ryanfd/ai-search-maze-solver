6183c3023ef400dedc19a135df715a82699063ce
Spenny1068 <spncrlll838@gmail.com>
2021-04-06 17:08:03 -0700

    patches set up

--- visualize.py
+++ visualize.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 from random import randrange
 import matplotlib.pyplot as plt
-from matplotlib import animation
+from matplotlib.animation import FuncAnimation
 from matplotlib.patches import Circle, Rectangle
 from matplotlib.patches import Rectangle
 from pathlib import Path
 # import numpy as np
+
+# # every 2d coordinate in the figure is a Rectangle()
+# # each Rectangle() is accessable by indexing the squares dictionary with the Rectangles coordinates
+# # Therefore, the squares dictionary holds the rectangle objects
+# # every 2d coordinate must be a matplotlib.patch, so that we can draw it on screen
+# # we shall set each patch as a square, so that we can index the square by the coordinate to get the underlying Rectangle() for that coordinate
+# for all squares in maze:
+#     square = plt.Rectangle((j, i))
+#     self.squares[(i, j)] = square
+#     self.patches.append(self.ax.add_patch(self.squares[(i, j)]))
+
+# # whenever we want to change a 2d coordinate on the plot, we use the setters/getters for the associated patch, which are actually squares, and since they are squares (which is a dictionary), the underlying Rectangle() is accessable
+# self.squares[(2, 2)].set_facecolor("red")
 
 
 class Cell(object):
@@ -71,7 +84,36 @@ class Visualize():
         self.cell_width = 1
         self.maze_obj = maze_obj
         self.animation = None
+        self.squares = None
         # self.maze_obj.maze = np.flip(np.transpose(self.maze_obj.maze), 1)
+
+    # create the base frame upon which the animation takes place
+    def initFrame(self):
+
+        self.squares = dict()
+        # {
+        #     # key: square
+        #     (0, 0): square_obj
+        #     (0, 1): square_obj
+        #     (0, 2): square_obj
+        # }
+
+        # initial maze: no solution paths
+        for i in range(self.maze_obj.num_rows):
+            for j in range(self.maze_obj.num_cols):
+                square = plt.Rectangle((j * self.cell_width, self.maze_obj.num_rows - (i * self.cell_width)), self.cell_width, self.cell_width, fc = self.maze_obj.maze[i][j].color, alpha = self.maze_obj.maze[i][j].alpha)
+                self.squares[(i, j)] = square
+                self.patches.append(self.ax.add_patch(self.squares[(i, j)]))
+
+        # must return self.patches, 
+        # it tells the animator update the patches object on the plot after each frame
+
+        # self.maze_obj.maze[2][2].color = "red"
+
+        # self.squares[(2, 2)].set_facecolor("red")
+
+        return self.patches
+
 
     def SetupFigure(self):
         """ setup the properties of the maze plot.
@@ -92,31 +134,31 @@ class Visualize():
         # self.ax.axes.get_yaxis().set_visible(False)
 
         # animate figure
-        self.animation = animation.FuncAnimation(self.fig, self.UpdateAnimation,
-                                                 frames=100,
-                                                 interval=100,
-                                                 blit=True)
+        # frames should equal length of solution path
+        self.animation = FuncAnimation(self.fig, 
+                                       self.UpdateAnimation,
+                                       init_func=self.initFrame,
+                                       frames=100,    # each frame of the animation
+                                       interval=100,
+                                       blit=True)
 
-    def UpdateAnimation(self, frame):
+        self.ShowFigure()
+
+    # updates animation every frame to show new maze figure
+    def UpdateAnimation(self, frameNumber):
+
+        print("updating...")
 
         rand_row = randrange(self.maze_obj.num_rows)
         rand_col = randrange(self.maze_obj.num_cols)
 
-        self.maze_obj.maze[rand_row][rand_col].color = "red"
-        self.UpdateFigure()
+        # print("rand_row = ", str(rand_row), ", rand_col = ", str(rand_col))
+        self.squares[(rand_row, rand_col)].set_facecolor("red")
 
+        # must return self.patches, 
+        # it tells the animaton framework to update the patches object on the plot
         return self.patches
 
-    def UpdateFigure(self):
-
-        self.patches.clear()
-        for i in range(self.maze_obj.num_rows):
-            for j in range(self.maze_obj.num_cols):
-                square = plt.Rectangle((j * self.cell_width, self.maze_obj.num_rows - (i * self.cell_width)), self.cell_width, self.cell_width, fc = self.maze_obj.maze[i][j].color, alpha = self.maze_obj.maze[i][j].alpha)
-                self.patches.append(square)
-
-        for p in self.patches:
-            self.ax.add_patch(p)
 
     def ShowFigure(self):
         plt.show()
@@ -125,7 +167,8 @@ class Visualize():
 my_maze = Maze()
 my_maze.GenerateMaze("maze_instances/maze2.txt")
 
-v = Visualize(my_maze)
-v.SetupFigure()
-v.UpdateFigure()
-v.ShowFigure()
+animation = Visualize(my_maze)
+animation.SetupFigure()
+animation.ShowFigure()
+# v.UpdateFigure()
+# v.ShowFigure()
